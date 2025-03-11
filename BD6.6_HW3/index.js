3f6e00794758a42cd5b82ee7f56fedef89664790
Sweety  Behera <40371480-sweetybehera283@users.noreply.replit.com>
2025-03-11 08:45:04 +0000

    modify vercel

---
+++ BD6.6_HW3/index.js
@@ -0,0 +1,25 @@
+const cors = require("cors");
+const express = require("express");
+
+const { getAllBooks, getBooksById } = require("./controllers");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/books", async (req, res) => {
+  const books = getAllBooks();
+  res.json({ books });
+});
+
+app.get("/books/details/:id", async (req, res) => {
+  let book = getBooksById(parseInt(req.params.id));
+
+  res.json({ book });
+});
+
+module.exports = { app };
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
---
+++ BD6.6_HW3/tests/api.test.js
@@ -0,0 +1,95 @@
+const request = require("supertest");
+const http = require("http");
+const { getAllBooks } = require("../controllers");
+const { app } = require("../index.js");
+
+jest.mock("../controllers", () => ({
+  ...jest.requireActual("../controllers"),
+  getAllBooks: jest.fn(),
+}));
+
+let server;
+
+beforeAll(async () => {
+  server = http.createServer(app);
+  server.listen(3001);
+});
+
+afterAll(async () => {
+  server.close();
+});
+
+describe("Controllers Function tests", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should return all employees", () => {
+    let mockbooks = [
+      {
+        bookId: 1,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        genre: "Fiction",
+      },
+      {
+        bookId: 2,
+        title: "1984",
+        author: "George Orwell",
+        genre: "Dystopian",
+      },
+      {
+        bookId: 3,
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        genre: "Classic",
+      },
+    ];
+    getAllBooks.mockReturnValue(mockbooks);
+    let result = getAllBooks();
+    expect(result).toEqual(mockbooks);
+    expect(result.length).toBe(3);
+  });
+});
+
+describe("API Endpoints test", () => {
+  it(" GET /books should get all books", async () => {
+    const res = await request(server).get("/books");
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      books: [
+        {
+          bookId: 1,
+          title: "To Kill a Mockingbird",
+          author: "Harper Lee",
+          genre: "Fiction",
+        },
+        {
+          bookId: 2,
+          title: "1984",
+          author: "George Orwell",
+          genre: "Dystopian",
+        },
+        {
+          bookId: 3,
+          title: "The Great Gatsby",
+          author: "F. Scott Fitzgerald",
+          genre: "Classic",
+        },
+      ],
+    });
+    expect(res.body.books.length).toBe(3);
+  });
+
+  it("GET /books/details/:id should get an books by ID", async () => {
+    const res = await request(server).get("/books/details/1");
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      book: {
+        bookId: 1,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        genre: "Fiction",
+      },
+    });
+  });
+});
